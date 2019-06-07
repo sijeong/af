8158163615ed911f73746479af7dc5eaa3c93a9c
sijeong <sijeong@outlook.com>
2019-06-07 13:31:23 +0900

    env

--- Avatar/ClientApp/src/environments/environment.ts
+++ Avatar/ClientApp/src/environments/environment.ts
@@ -1,9 +1,26 @@
+import { WebStorageStateStore } from "oidc-client";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  oidc_conf: {
+    oidc_config: {
+      authority: 'https://localhost:44385',
+      client_id: 'ng-oidc-client-identity',
+      response_type: 'id_token token',
+      scope: 'openid profile offline_access api1',
+      redirect_uri: 'http://localhost:4200/callback.html',
+      silent_redirect_uri: 'http://localhost:4200/renew-callback.html',
+      post_logout_redirect_uri: 'http://localhost:4200/signout-callback.html',
+      accessTokenExpiringNotificationTime: 10,
+      automaticSilentRenew: true,
+      userStore: () => new WebStorageStateStore({ store: window.localStorage })
+    }
+  }
+
 };
 
 /*
