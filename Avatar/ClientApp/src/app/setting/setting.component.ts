75ed477998121a73749c8c7f216658a20739d81a
sijeong <sijeong@outlook.com>
2019-06-18 07:24:14 +0900

    cmd filter & minor updates

--- Avatar/ClientApp/src/app/setting/setting.component.ts
+++ Avatar/ClientApp/src/app/setting/setting.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { AppState } from '../root-store';
-import { UpsertCommands } from './store/command.actions';
+import { UpsertCommands, DeleteCommands, ClearCommands } from './store/command.actions';
 import { routes as appRoutes } from '../app-routing.module'
+import { selectAllCommands } from './store/command.reducer';
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
@@ -14,9 +15,17 @@ export class SettingComponent implements OnInit {
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.routes = appRoutes.map(ar => ar.path);
-  }
+    this.cmd$.subscribe(c => this.commands = c.map(x => x.data));
 
+    this.routes = appRoutes
+      .map(ar => ar.path)
+      .filter((value, index, array) => {
+        return !value.includes(':')
+      }).filter(n => !this.commands.includes(n))
+  }
+  cmd$ = this.store.pipe(
+    select(selectAllCommands)
+  )
   routes = [
     'home',
     'article',
@@ -42,6 +51,7 @@ export class SettingComponent implements OnInit {
 
   save() {
     const cmds = this.commands.map((c, index) => { return { id: index, data: c } })
+    this.store.dispatch(new ClearCommands());
     this.store.dispatch(new UpsertCommands({ commands: cmds }));
   }
 
